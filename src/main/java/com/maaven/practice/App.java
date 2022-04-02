33228983ba1ffecc11794ecc5e8f99755cf64e56
junaid526 <80666143+junaid526@users.noreply.github.com>
2022-04-02 17:31:25 +0530

    Update App.java
    commit on println 11

--- src/main/java/com/maaven/practice/App.java
+++ src/main/java/com/maaven/practice/App.java
@@ -19,5 +19,6 @@ public class App
         System.out.println( "Hello World8!" );
         System.out.println( "Hello World9!" );
         System.out.println( "Hello World10!" );
+        System.out.println( "Hello World11!" );
     }
 }
